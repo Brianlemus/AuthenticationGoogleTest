189027b8a2c54c5de59812d3f2858d82e6c3d045
Blemus <PERSONAL@DESKTOP-AP8QVV2>
2024-09-18 00:21:26 -0600

    Iniciando proyecto cargado solo con id cliente y secret configurado

---
+++ AuthGoggleUMG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthGoggleUMG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
